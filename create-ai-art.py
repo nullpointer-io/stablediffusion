7f883b26757780a1d105592fd0d0249234569f6d
nullpointer-io <83554262+nullpointer-io@users.noreply.github.com>
2022-08-27 17:41:53 +0200

    Update create-ai-art.py

--- create-ai-art.py
+++ create-ai-art.py
@@ -1,1 +1,41 @@
+#!/usr/bin/env python3
 
+import fire
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+
+MODEL = "CompVis/stable-diffusion-v1-4"
+GPU = "cuda"
+
+def slugify(value, allow_unicode=False):
+    """
+    Taken from https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
+
+def main(prompt, over10gb=True):
+   
+    fp16args = {}
+    if not over10gb:
+        fp16args = { "torch_dtype": torch.float16, "revision": "fp16" }  
+    
+    pipe = StableDiffusionPipeline.from_pretrained(MODEL, use_auth_token=True, **fp16args)
+    pipe = pipe.to(GPU)
+
+    with autocast("cuda"):
+        image = pipe(prompt, guidance_scale=7.5)["sample"][0] 
+    image.save(f"{slugify(prompt)}.png")
+
+if __name__ == '__main__':
+    fire.Fire(main)
