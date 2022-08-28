4c714480f1821f01abe52f3f8c85255e2b3c0042
se <se@pit2.home.erben.it>
2022-08-28 11:46:29 +0200

    Collect output in subdir and save parameter info to csv

--- stablediffusion.py
+++ stablediffusion.py
@@ -1,13 +1,59 @@
 #!/usr/bin/env python3
 
-import fire
+import csv
+from pathlib import Path
 import re
+import shutil
+from time import localtime, strftime, time 
+
 from diffusers import StableDiffusionPipeline
+import fire
+import pandas as pd
 import torch
 import unicodedata
 
 MODEL = "CompVis/stable-diffusion-v1-4"
 GPU = "cuda"
+CSV_FILENAME = "stablediffusion.info.csv"
+OUTDIR = Path.cwd() / 'collected'
+
+def get_imgfile_from_prompt(idx, textinput):
+    return OUTDIR / f"{idx:05}-{slugify(textinput)}.png"
+
+def save_info(**params):
+    
+    csvfile = OUTDIR / CSV_FILENAME
+    write_mode = 'a' if csvfile.exists() else 'w'  
+    header = False if write_mode == 'a' else True
+    
+    col2params = {'prompt': 'textinput', 'num_inference_steps': 'steps', 
+                  'manual_seed': 'manualseed', 'generator_seed': 'seed',
+                  'guidance_scale': 'scale'}
+    
+    if csvfile.exists():
+        current = pd.read_csv(csvfile)
+        idx = int(current.idx.max()) + 1
+    else:
+        idx = 0
+    
+    imgfile = get_imgfile_from_prompt(idx, params['textinput'])
+    
+    data = dict()
+    data['idx'] = idx 
+    data['filename'] = imgfile.name
+    for colname in col2params.keys(): 
+        paramname = col2params[colname]
+        data[colname] = [params[paramname]]
+    data['timestamp'] = int(time())
+    
+    df = pd.DataFrame(data)
+    df.to_csv(csvfile, header=header, index=False, mode=write_mode, quoting=csv.QUOTE_NONNUMERIC)
+    
+    csv_status = "created" if write_mode == 'w' else "updated"
+    print(f"CSV {csv_status}: {csvfile}")
+
+    return imgfile
+    
 
 def slugify(value, allow_unicode=False):
     """
@@ -27,28 +73,30 @@ def slugify(value, allow_unicode=False):
 
 def main(textinput, steps=15, manualseed=True, seed=1024, scale=7.5, over10gb=True):
 
-    fp16args = dict()
+    #import pydevd; pydevd.settrace()
+    params = dict(**locals())   # needed for saving into info file
+
     if not over10gb:
-        fp16args = {"torch_dtype": torch.float16, "revision": "fp16"}  # pylint: disable=no-member
+        params.update({"torch_dtype": torch.float16, "revision": "fp16"})  # pylint: disable=no-member
+        params.pop(over10gb)    # should not be in info file
+
+    OUTDIR.mkdir(exist_ok=True)
 
     if not manualseed:
         generator = torch.Generator(GPU).seed() # pylint: disable=no-member
     else:
         generator = torch.Generator(GPU).manual_seed(seed)  # pylint: disable=no-member
-    pipe = StableDiffusionPipeline.from_pretrained(MODEL, generator=generator, guidance_scale=scale,
-                                                   num_inference_steps=steps, use_auth_token=True, 
-                                                   **fp16args)
+    pipe = StableDiffusionPipeline.from_pretrained(MODEL, generator=generator, use_auth_token=True, **params)
     pipe = pipe.to(GPU)
 
     with torch.autocast(GPU):
         result = pipe(textinput) 
         print(f"Result: {result}")
-        #image = pipe(textinput, guidance_scale=7.5)["sample"][0] 
         img = result["sample"][0]
-    
-    imgname = f"{slugify(textinput)}.png"
-    img.save(imgname)
-    print(f"SUCCESS: {imgname}")
+
+    imgfile = save_info(**params)
+    img.save(imgfile)
+    print(f"SUCCESS: {imgfile}")
 
 if __name__ == '__main__':
     fire.Fire(main)
